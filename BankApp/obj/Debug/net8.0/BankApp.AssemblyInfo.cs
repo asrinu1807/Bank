0a9f3383481494e2c83164e06eab72b3a9aa95fc
Srinivas <srinivas@gmail.com>
2024-02-23 06:50:38 +0000

    created Cart model

--- BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ BankApp/obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf0111b6331563c0967696586ec563646420098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe1f949b1a20601033aea079bcd3aace7f954f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
